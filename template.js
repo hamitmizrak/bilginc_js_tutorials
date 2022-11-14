d71625ff5ef5143041d71db7bc8813e0823d1abb
hamitmizrak <hamitmizrak@gmail.com>
2022-11-14 10:13:42 +0300

    first commit

---
+++ template.js
@@ -0,0 +1,26 @@
+/* 
+console.log("console.log data")
+document.write("deneme<br/>")
+alert("deneme")
+""
+''
+``
+*/
+//dynamics variable
+//global scope
+//var
+var value1 = "Data";
+document.write(value1 + "<br/>")
+var value1 = "Data";
+
+//local scope
+//let:
+let $_value2 = "Data2";
+alert($_value2)
+let value3 = "Data2";
+value3 = 44;
+
+//const
+const value4 = 3.14;
+
+
