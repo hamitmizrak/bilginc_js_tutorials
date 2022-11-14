1abc6258a2689b035f20a04fd20f4672f4c96e50
hamitmizrak <hamitmizrak@gmail.com>
2022-11-14 10:47:10 +0300

    js continues

--- template.js
+++ template.js
@@ -9,18 +9,59 @@ alert("deneme")
 //dynamics variable
 //global scope
 //var
-var value1 = "Data value 55 66";
-document.write(value1 + "<br/>")
-var value1 = "Data";
+// var valueData = "Data value 55 66";
+// document.write(valueData + "<br/>")
+// var value1 = "Data";
 
-//local scope
-//let:
-let $_value2 = "Data2";
-alert($_value2)
-let value3 = "Data2";
-value3 = 44;
+// //local scope
+// //let:
+// let $_value2 = "Data2";
+// alert($_value2)
+// let value3 = "Data2";
+// value3 = 44;
 
-//const
-const value4 = 3.14;
+// //const
+// const value4 = 3.14;
+//let value1 = 44;
+// let value2 = 44.56;
+// let value3 = 3 > 1;
+// let value4 = [];
+// let value5 = {};
+
+// //whoisting
+// value6="Merhabalar";
+// let value6;
+
+//typeof
+// let value1 = 44;
+// console.log(typeof value1);
+
+// let value2 = 44.56;
+// console.log(typeof value2);
+
+// let value3 = "Merhablar";
+// console.log(typeof value3)
+
+// let value4 = true;
+// console.log(typeof value4)
+
+// let value5 = [];
+// console.log(typeof value5)
+
+// let value6 = {};
+// console.log(typeof value6)
+
+//undefined
+// let data;
+// console.log(data);
+
+// //Infinity
+// const data2=44/0;
+// console.log(data2)
+
+// //NaN
+// let data3=Number("asd")/14;
+// console.log(data3)
+
 
 
